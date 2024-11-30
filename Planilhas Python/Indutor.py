0545d5de7760ad76ae7566a23a0e7eb57ac64b6d
GabrrielGSB <g.gonzaga.barreto@gmail.com>
2024-11-30 08:54:46 -0300

    Update Indutor.py

--- Planilhas Python/Indutor.py
+++ Planilhas Python/Indutor.py
@@ -1,46 +1,110 @@
 from imports import *
+
 def sqrt(x):
     return m.sqrt(x)
 def ceil(x):
     return m.ceil(x)
 
 class Indutor:
-    def __init__(self):
-        self.tensaoEntrada = 0
-        self.tensaoSaida   = 0
-        self.potenciaSaida = 0
-        self.frequencia    = 0
-        self.correnteSaida = 0
-        self.rendimento    = 0
-        self.regulacao     = 0
-        self.densiFluxo    = 0
-        self.deltaTemp     = 0
-        self.densiCorrente = 0
-        self.usoJanela     = 0
-        self.fatorForma    = 0
+    def __init__(self,  tensaoEntrada=1, 
+                        tensaoSaida=1,
+                        potenciaSaida=1,
+                        frequencia=1,
+                        correnteSaida=1,
+                        rendimento=1,
+                        regulacao=1,
+                        densiFluxo=1,
+                        deltaTemp=1,
+                        densiCorrente=1,
+                        usoJanela=1,
+                        formaOnda='senoide'):
+        
+        self.tensaoEntrada = tensaoEntrada #[V]
+        self.tensaoSaida   = tensaoSaida   #[V]
+        self.potenciaSaida = potenciaSaida #[W]
+        self.frequencia    = frequencia    #[Hz]
+        self.correnteSaida = correnteSaida #[A]
+        self.rendimento    = rendimento    #[η]
+        self.regulacao     = regulacao     #[α]
+        self.densiFluxo    = densiFluxo    #[T]
+        self.deltaTemp     = deltaTemp     #[°C]
+        self.densiCorrente = densiCorrente #[A/cm2]
+        self.usoJanela     = usoJanela     #[Ku]
+        self.fatorForma    = 0             #[Kf]
+        self.formaOnda     = formaOnda 
 
-        self.AWGpri             = 0
         self.OhmPorCm           = 0
         self.OhmPorCmSec        = 0
-        self.alamina_cm2        = 0 
-        self.alamina_inch2      = 0
         self.areaSuperficie     = 0
-        self.wattKilo_Aperam    = 0
-        self.empilhamento_cm    = 0
-        self.empilhamento_inch  = 0
-        self.comprimentoFioPri  = 0
+        self.wattKiloAperam     = 0
         self.comprimentoFioSec  = 0
         self.resistividadeCobre = 68.5  #[μΩ/cm] p/ 100°C (constante)
         self.Ae, self.Aw, self.Ap  = 0,0,0 
     
-    # def sequenciaCalculo(self):
+    def calculoProjetoTransformador(self):
+        self.calcularPotenciaTotal()
+        self.calcularProdutoArea()
+        self.introducao()
+        self.definirAw()
+        self.definirAe()
+        self.definirApChapa()
+        self.calcularNumEspirasPri()
+        self.verificarDensiCorrente()
+        self.calcularCorrenteEntrada()
+        self.calcularSecaoFioPri()
+        self.calculoNumFiosParalelosPri()
+        self.definirComprimentoEspiraPri()
+        self.calculoResistenciaPri()
+        self.calculoPerdasCobrePri()
+        self.calculoNumEspirasSec()
+        self.calcularSecaoFioSec()
+        self.definirAreaFioSecundario_cm2()
+        self.calculoNumFiosParalelosSec()
+        self.calculoResistenciaSec()
+        self.calculoPerdasCobreSec()
+        self.calculoPerdasTotaisCobre()
+        self.verificarRegulacaoTensao()
+        self.definirPerdaWattKilo()
+        self.definirDimensoesNucleo()
+        self.calculoPerdasAcoSilicio()
+        self.calculoPerdasTotais()
 
-    def definirAWG_primario(self, AWG):
-        self.AWGpri = AWG
 
-    def definirAWG_secundario(self, AWG):
-        self.AWGsec = AWG
+
+    def introducao(self):
+        print("Defina algumas constantes tabeladas com base em Ap calculado. Sendo...")
+        print(f"Ap = {self.produtoArea} [cm^4]")
+
+    def definirAw(self):
+        self.Aw = input("Área da Janela(Aw) escolhida: ")
+
+    def definirAe(self):
+        self.Ae = input("Área da seção transversal(Ae) escolhida: ")
+
+    def definirApChapa(self):
+        self.ApChapa = self.Aw * self.Ae
+
+    def definirAWGprimario(self):
+        self.AWGpri = input("O AWG escolhido para o primário é: ")
+
+    def definirAWGsecundario(self):
+        self.AWGsec = input("O AWG escolhido para o secundário é: ")
     
+    def definirComprimentoEspiraPri(self):
+        self.comprimentoFioPri = input("O comprimento da espira(MLT) primária escolhida é: ")
+    
+    def definirComprimentoEspiraSec(self):
+        self.comprimentoFioSec = input("O comprimento da espira(MLT) secundária escolhida é: ")
+
+    def definirPerdaWattKilo(self):
+        self.wattKiloAperam = input("A perda em W/Kg Tabelada para o núcleo escolhido é: ")
+    
+    def definirDimensoesNucleo(self):
+        self.areaLamina_cm2    = input("Definir Área da Lámina do transformador [cm^2]:  ")
+        self.empilhamento_cm   = input("Definir Empilhamento de Láminas do transformador [cm]: ")
+        self.areaLamina_inch2  = self.areaLamina_cm2 * 0.155
+        self.empilhamento_inch = self.empilhamento_cm / 2.54
+
     def definirAreaFioPrimario_cm2(self, mm2):
         self.areaFioPri_mm2 = mm2
         self.areaFioPri_cm2 = self.areaFioPri_mm2 / 100
@@ -56,7 +120,7 @@ class Indutor:
         self.produtoArea = ((self.potenciaTotal * 1e4) / 
                             (self.densiFluxo * self.frequencia * self.densiCorrente * self.usoJanela * self.fatorForma))  
         
-    def calcularEspirarPri(self):
+    def calcularNumEspirasPri(self):
         self.numEspirasPri   = int(((self.tensaoEntrada * 1e4) / 
                                     (self.fatorForma * self.densiFluxo * self.frequencia * self.Ae)))
 
@@ -111,7 +175,7 @@ class Indutor:
 
     def calculoPerdasAcoSilicio(self):
         self.pesoEstimado = self.alamina_inch2 * self.empilhamento_inch * 0.125
-        self.perdaFerro   = self.wattKilo_Aperam * self.pesoEstimado
+        self.perdaFerro   = self.wattKiloAperam * self.pesoEstimado
     
     def calculoPerdasTotais(self):
         self.perdaTotal = self.perdasTotaisCobre + self.perdaFerro
