7a4c9fce515dc80c2a889c261a1f8b91c40a90cd
GabrrielGSB <g.gonzaga.barreto@gmail.com>
2024-12-02 22:35:47 -0300

    Update main.py

--- Planilhas Python/main.py
+++ Planilhas Python/main.py
@@ -1,18 +1,34 @@
-from Indutor import Indutor
+from Transf import Transformador
 
-I = Indutor(tensaoEntrada = 400,
-            tensaoSaida   = 54,
-            correnteSaida = 9.26,
-            potenciaSaida = 500,
-            frequencia    = 100e3,
-            rendimento    = 0.98,
-            regulacao     = 0.5,
-            densiFluxo    = 0.05,
-            deltaTemp     = 30,
-            densiCorrente = 250,
-            usoJanela     = 0.4,
-            dutyCicle     = 0.4,
-            formaOnda     = 'quadrada',
-            tipoTransf    = 'tipo 1')
+# I = Indutor(tensaoEntrada = 400,
+#             tensaoSaida   = 54,
+#             correnteSaida = 9.26,
+#             potenciaSaida = 500,
+#             frequencia    = 100e3,
+#             rendimento    = 0.98,
+#             regulacao     = 0.5,
+#             densiFluxo    = 0.05,
+#             deltaTemp     = 30,
+#             densiCorrente = 250,
+#             usoJanela     = 0.4,
+#             dutyCicle     = 0.4,
+#             formaOnda     = 'quadrada',
+#             tipoTransf    = 'tipo 1')
 
-I.calcularProjetoTransformadorAltaFreq()
+transformador = Transformador(tensaoEntrada  =220,
+                              tensaoSaida    =220,
+                              correnteSaida  =9.1,
+                              potenciaSaida  =2000,
+                              frequencia     =60,
+                              rendimento     =0.95,
+                              regulacao      =5,
+                              densiFluxo     =1,
+                              deltaTemp      =30,
+                              densiCorrente  =450,
+                              usoJanela      =0.4,
+                              formaOnda      ="senoide",
+                              tipoTransf     ="tipo 3")
+
+transformador.calcularProjeto()
+
+
